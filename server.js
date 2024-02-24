74c7d4c2748a9413d3014cd950ded044ec37ec39
Hritik-khantwal <hritikkhantwal25@gmail.com>
2024-02-24 23:43:29 +0530

    clg in server __dirname

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ connectDB();
 //esmodule fix
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+console.log(__dirname.bgCyan.black);
 
 // rest object
 const app = express();
