fed1bf487ad1563886ff3b733e1d0833bd5ac485
Hritik-khantwal <hritikkhantwal25@gmail.com>
2024-02-25 16:23:41 +0530

    updated server

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ import cors from "cors";
 import categoryRoutes from "./routes/CategoryRoutes.js";
 import productRoutes from "./routes/productRoute.js";
 import ratingRoutes from "./routes/ratingRoute.js";
-import path from "path";
-import { fileURLToPath } from "url";
+// import path from "path";
+// import { fileURLToPath } from "url";
 
 // configure env
 dotenv.config();
@@ -18,9 +18,9 @@ dotenv.config();
 connectDB();
 
 //esmodule fix
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-console.log(__dirname.bgCyan.black);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// console.log(__dirname.bgCyan.black);
 
 // rest object
 const app = express();
@@ -29,7 +29,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "./client/build")));
+// app.use(express.static(path.join(__dirname, "./client/build")));
 
 // routes
 app.use("/api/v1/auth", authRoutes);
@@ -37,13 +37,13 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 app.use("/api/v1/rating", ratingRoutes);
 // rest api
-app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
-});
-
-// app.get("/", (req, res) => {
-//   res.send("<h1>server is working </h1>");
+// app.use("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "client/build/index.html"));
 // });
+
+app.get("/", (req, res) => {
+  res.send("<h1>server is working </h1>");
+});
 
 // PORT
 const PORT = process.env.PORT || 8080;
